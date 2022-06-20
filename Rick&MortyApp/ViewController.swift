b830e709050e42bf004607289c24a8f19b4f2883
Uğur Can Gedik <canugurgedik@gmail.com>
2022-06-20 10:03:30 +0300

    Initial Commit

---
+++ Rick&MortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&MortyApp
+//
+//  Created by Uğur Can Gedik on 20.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
